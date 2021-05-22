5b2761e7c04ecd5bce9388490631fee7ddec6fac
Jason Wong <jason.wong@districtrace.com>
2021-05-22 18:06:22 +0800

    chore: Add folder aliases

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,15 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    plugins: ['react-native-reanimated/plugin', [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          view: './src/view',
+          styles: './src/styles'
+        },
+      },
+    ],]
   };
 };
